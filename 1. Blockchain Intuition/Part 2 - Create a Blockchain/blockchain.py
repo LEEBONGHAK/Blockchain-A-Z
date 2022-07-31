40db821b13efbb8e8b75bc8ec1583c31b4826772
octo <dlqhdgkr456@gmail.com>
2022-07-31 17:14:29 +0900

    step 4

--- 1. Blockchain Intuition/Part 2 - Create a Blockchain/blockchain.py
+++ 1. Blockchain Intuition/Part 2 - Create a Blockchain/blockchain.py
@@ -8,6 +8,7 @@
 import datetime  # 블록이 생성되고 채굴된 타임스탬프를 위해 사용
 import hashlib  # 블록을 해시하기 위해 사용
 import json  # 블록을 해시하기 전에 블록 인코딩을 위해 사용
+
 # Flask: 웹 애플리케이션이 되는 Flask 객체를 생성을 위해, jsonify: Postman에서 블록체인과 상호 작용할 때 메세지를 보내기 위해
 from flask import Flask, jsonify
 
@@ -19,5 +20,16 @@ class Blockchain:
     def __init__(self):
         self.chain = []  # 블록이 포함될 체인 초기화
         self.create_block(proof=1, previous_hash='0')  # genesis block 생성
+
+    def create_block(self, proof, previous_hash):  # 블록생성 메서드
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash
+        }
+        self.chain.append(block)
+        return block
+
 
 # Part 2 - Mining our Blockchain
